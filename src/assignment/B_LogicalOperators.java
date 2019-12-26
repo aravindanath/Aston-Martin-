f2bf4bfae59d8120fa60e6a7bd9a10f1a7d28676
bharathi-yernagula <bharathi.yernagula@gmail.com>
2019-12-26 21:06:18 +0530

    GIT Commit for Calculator

---
+++ src/assignment/B_LogicalOperators.java
@@ -0,0 +1,34 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class B_LogicalOperators {
+	
+	public static void main(String[] args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three unique numbers");
+		int i =sc.nextInt();
+		int j =sc.nextInt();
+		int k =sc.nextInt();
+		
+		if (i > j && i > k)
+			System.out.println(i +" is greatest");
+		else if (j>i && j >k)
+			System.out.println(j +" is greatest");
+		else if (k>i && k > j)
+			System.out.println(k +" is greatest");
+		
+		int x=10 ;
+		int y=20 ;
+		int z=30 ;
+		
+		if (x < y || x <z )
+			System.out.println(x +" is smallest of all the numbers");
+				
+		
+		
+		
+	}
+	
+}
