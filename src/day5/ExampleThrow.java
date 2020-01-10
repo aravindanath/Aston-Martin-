e38339929073b1f9c8f562c7c25dfa9b10313ee7
aravindanath <aravindanath86@gmail.com>
2020-01-10 09:03:08 +0530

    example

--- src/day5/ExampleThrow.java
+++ src/day5/ExampleThrow.java
@@ -2,11 +2,11 @@ package day5;
 
 public class ExampleThrow {
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws ArithmeticException {
 		int amt = 50;
 		
 		if(amt <100) {
-			throw new Exception("Enter > 100");
+			throw new ArithmeticException("Enter > 100");
 		}else {
 			System.out.println("With draw..");
 		}
