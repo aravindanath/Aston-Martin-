e72d5ebef94bfb8e83e8e2c2a9a8dffb7608d9c5
ArshaMohan72 <arshamohan72@gmail.com>
2020-01-14 08:49:22 +0530

    relative path

---
+++ src/day6/CreateNewFile_Arsha.java
@@ -0,0 +1,11 @@
+package day6;
+
+public class CreateNewFile_Arsha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String relativePath = System.getProperty("user.dir");
+        System.out.println(relativePath);
+	}
+
+}
